e018ecc2c563a26ddfb4c5ba611d83d12f823f94
Jingyi Shu <jingyi.shu@alumnos.upm.es>
2026-09-11 20:06:35 +0200

    feat: add GET /user/{id} endpoint

--- src/main/java/es/upm/miw/devops/rest/UserResource.java
+++ src/main/java/es/upm/miw/devops/rest/UserResource.java
@@ -1,4 +1,23 @@
 package es.upm.miw.devops.rest;
 
+import es.upm.miw.devops.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(UserResource.USERS)
 public class UserResource {
+    public static final String USERS = "/users";
+
+    private final UserService userService;
+
+    @Autowired
+    public UserResource(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public Object read(@PathVariable String id) {
+        return this.userService.read(id);
+    }
 }
--- src/main/java/es/upm/miw/devops/services/UserService.java
+++ src/main/java/es/upm/miw/devops/services/UserService.java
@@ -1,4 +1,13 @@
-package es.upm.miw.devops.rest.services;
+package es.upm.miw.devops.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    // 如果已有 UserRepository，在此处注入；没有则先返回模拟数据或对接 Repository
+    public Object read(String id) {
+        // TODO: 调用 userRepository.findById(id)
+        return null;
+    }
 }
